a02fab8d96d5690ce9d1a058830a0f0357578a65
Morvin-Ian <morvinian@gmail.com>
2024-09-24 23:16:19 +0300

    finalizing on User model

--- config/settings/base.py
+++ config/settings/base.py
@@ -137,6 +137,9 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
 import logging
 import logging.config
 
