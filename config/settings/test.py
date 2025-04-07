0ed43d89d848da9c19330ac27356eef9a0862019
Morvin-Ian <morvinian@gmail.com>
2025-04-07 22:09:52 +0300

    test

--- config/settings/test.py
+++ config/settings/test.py
@@ -14,3 +14,4 @@ DATABASES = {
 
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+DEFENCE="Red"
