5d08867914629871b6bee128b6c1c03068470091
Morvin-Ian <morvinian@gmail.com>
2025-06-01 21:08:39 +0300

    team api finalizing

--- apps/fantasy/api/serializers.py
+++ apps/fantasy/api/serializers.py
@@ -1,10 +1,13 @@
+from apps.accounts.models import User
 from rest_framework import serializers
+from rest_framework import status
 from apps.kpl.models import Player, Gameweek
 from apps.kpl.api.serializers import PlayerSerializer, TeamSerializer
 from apps.fantasy.models import (
     FantasyTeam
 )
 
+from util.errors.exception_handler import CustomInternalServerError
 
 class FantasyTeamSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
@@ -15,5 +18,27 @@ class FantasyTeamSerializer(serializers.ModelSerializer):
         read_only_fields = ('user', 'total_points', 'overall_rank', 'budget', 'gameweek', 'free_transfers', 'transfer_budget')
 
     def create(self, validated_data):
-        user = self.context['request'].user
-        return FantasyTeam.objects.create(user=user, **validated_data)
+        try:
+            team = FantasyTeam.objects.filter(user__id=self.context['request'].user.id)
+            user = User.objects.get(id=self.context['request'].user.id)
+        
+            if team.exists():    
+                raise CustomInternalServerError(
+                    message="You already have a fantasy team.",
+                    status_code=status.HTTP_400_BAD_REQUEST  
+                )
+            team_name = validated_data.get('name')
+            if FantasyTeam.objects.filter(name__iexact=team_name).exists():
+                raise CustomInternalServerError(
+                    message=f"A team with the name '{team_name}' already exists.",
+                    status_code=status.HTTP_400_BAD_REQUEST
+                )
+                
+            return FantasyTeam.objects.create(user=user, **validated_data)
+        except CustomInternalServerError as api_exec:
+            raise api_exec
+        except Exception as e:
+            raise CustomInternalServerError(
+                message=str(e),
+                status_code=500
+            )
--- apps/fantasy/api/urls.py
+++ apps/fantasy/api/urls.py
@@ -1,9 +1,6 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import FantasyTeamViewSet
 
-
-urlpatterns = [
-    path("teams/", FantasyTeamViewSet.as_view({"get": "list"}), name="fantasy-team-list"),
-    path("teams/<uuid:pkid>/", FantasyTeamViewSet.as_view({"get": "retrieve"}), name="fantasy-team-detail"),
-]   
+router = DefaultRouter()
+router.register(r'teams', FantasyTeamViewSet, basename='fantasy-teams')
+urlpatterns = router.urls
