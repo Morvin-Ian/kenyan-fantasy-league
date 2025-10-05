b48c607445d85879e0a49fb0ad015c1f38fee242
Morvin-Ian <morvinian@gmail.com>
2025-10-05 14:29:34 +0300

    minor updates

--- util/selenium.py
+++ util/selenium.py
@@ -27,6 +27,11 @@ class SeleniumManager:
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        )
 
         try:
             self.driver = webdriver.Remote(
